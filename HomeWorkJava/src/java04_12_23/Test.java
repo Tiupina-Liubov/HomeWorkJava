fd6837e74057f8916f9f96562bf4e067e7b16d75
Tiutina_Liubov <kyzjaxxx93@gmail.com>
2023-12-07 18:13:53 +0100

    Home work 04.12.23

--- HomeWorkJava/src/java04_12_23/Test.java
+++ HomeWorkJava/src/java04_12_23/Test.java
@@ -27,12 +27,12 @@ public class Test {
 //       3 HashMap<Integer, List<String>>: Для каждого ключа выведите все строки из соответствующего списка,
 //       которые начинаются с гласной буквы.
         Map<Integer, List<String>> str = new HashMap<>();
-        str.put(1, new ArrayList<>(Arrays.asList("Hallo", "main", "Liubov", "Tiupina")));
-        str.put(2, new ArrayList<>(Arrays.asList("hallo", "Main", "liuv", "Tiupna")));
-        str.put(9, new ArrayList<>(Arrays.asList("hallo", "Main", "iola", "Tiuina")));
+        str.put(1, new ArrayList<>(Arrays.asList("Aallo", "main", "Liubov", "Tiupina")));
+        str.put(2, new ArrayList<>(Arrays.asList("hallo", "Main", "iuda", "Tiupna")));
+        str.put(9, new ArrayList<>(Arrays.asList("allo", "ani", "iola", "Tiuina")));
         str.put(3, new ArrayList<>(Arrays.asList("allo", "main", "Liubov", "tiupinana")));
-        str.put(4, new ArrayList<>(Arrays.asList("lo", "main", "Liov", "Tipina")));
-
+        str.put(4, new ArrayList<>(Arrays.asList("you", "main", "iov", "Tipina")));
+        meaningsThatBeginWithVowel(str);
 
         //4 HashMap<Integer, Integer>: Найдите ключ с максимальным значением и выведите эту пару.
         Map<Integer, Integer> numbers = new HashMap<>();
@@ -45,6 +45,7 @@ public class Test {
         numbers.put(0, 4);
         numbers.put(2, 6);
         maximumKeyValue(numbers);
+
 //        5.HashMap<String, String>: Определите, содержит ли HashMap такую пару ключ-значение, где ключ является
 //        обратной строкой значения (например, ключ "abc", значение "cba").
         Map<String, String> stringMap = new HashMap<>();
@@ -56,6 +57,7 @@ public class Test {
         stringMap.put("fire", "erif");
         stringMap.put("bumm", "mumb");
         keyReverseStringValue(stringMap);
+
 //       6. HashMap<String, Integer>: Переберите HashMap и преобразуйте его в HashMap<String, String>,
 //        где каждое значение - это строковое представление исходного числа, умноженного на 2.
         Map<String, Integer> stringIntegerMap = new HashMap<>();
@@ -67,97 +69,120 @@ public class Test {
         stringIntegerMap.put("6", 5);
         stringIntegerMap.put("7", 4);
         stringIntegerMap.put("8", 65);
-        Map<String,String>newStringMap=stringRepresentationNumberMultiplied2(stringIntegerMap);
+        Map<String, String> newStringMap = stringRepresentationNumberMultiplied2(stringIntegerMap);
         System.out.println(newStringMap);
+
 //       7. HashMap<Integer, List<String>>: Создайте второй HashMap<Integer, Integer>, где каждый ключ - это ключ из
 //        исходного HashMap, а значение - общее количество символов во всех строках списка этого ключа.
         System.out.println(str);
-        Map<Integer,Integer> lengthListStrings=new HashMap<>();
-        lengthListStrings=valueLengthStrings(str);
+        Map<Integer, Integer> lengthListStrings = new HashMap<>();
+        lengthListStrings = valueLengthStrings(str);
         System.out.println(lengthListStrings);
+
 //        8 HashMap<Integer, Integer>: Найдите все пары ключ-значение, где ключ минус значение является простым числом.
         System.out.println(numbers);
         findingSubtractionKeyValue(numbers);
+
 //        9 HashMap<String, String>: Определите, можно ли преобразовать HashMap в List<String>, где каждый элемент -
 //        это конкатенация ключа и значения, так, чтобы все элементы списка были уникальными.
-       List<String>concatenationStrings= keyValueConcatenation(stringMap);
+        List<String> concatenationStrings = keyValueConcatenation(stringMap);
         System.out.println(concatenationStrings);
+
 //        10 HashMap<String, Integer> и HashMap<Integer, List<String>>: Для каждого ключа в HashMap<String, Integer>
 //        найдите соответствующие строки в HashMap<Integer, List<String>>, где ключ во втором HashMap - это значение из
 //        первого, и выведите их.
-        Map<String,Integer> stringIntegerMap1= new HashMap<>();
-        stringIntegerMap1.put("A",1);
-        stringIntegerMap1.put("B",2);
-        stringIntegerMap1.put("C",3);
-        stringIntegerMap1.put("D",4);
-        stringIntegerMap1.put("E",5);
-        stringIntegerMap1.put("F",6);
-        stringIntegerMap1.put("J",7);
-        Map<Integer,List<String>> stringMap1= new HashMap<>();
-        stringMap1.put(7, Arrays.asList("Liubov","Anna","Katya"));
-        stringMap1.put(6, Arrays.asList("Kola","Pepsi","Fanta"));
-        stringMap1.put(5, Arrays.asList("Liubov","Nadya","Katya"));
-        stringMap1.put(4, Arrays.asList("Kris","Anna","Kola"));
-        stringMap1.put(3, Arrays.asList("Liuba","Anna","Andrey"));
-        stringMap1.put(2, Arrays.asList("Vasa","Anna","Katya"));
-        stringMap1.put(1, Arrays.asList("Ludmila","Anna","Kira"));
+        Map<String, Integer> stringIntegerMap1 = new HashMap<>();
+        stringIntegerMap1.put("Anna", 1);
+        stringIntegerMap1.put("E", 2);
+        stringIntegerMap1.put("Cing", 3);
+        stringIntegerMap1.put("Dima", 3);
+        stringIntegerMap1.put("Kira", 5);
+        stringIntegerMap1.put("F", 6);
+        stringIntegerMap1.put("J", 7);
+        Map<Integer, List<String>> stringMap1 = new HashMap<>();
+        stringMap1.put(7, Arrays.asList("Liubov", "Anna", "Katya"));
+        stringMap1.put(6, Arrays.asList("Kola", "Pepsi", "Fanta"));
+        stringMap1.put(5, Arrays.asList("Liubov", "Nadya", "Katya"));
+        stringMap1.put(4, Arrays.asList("Kris", "Dascha", "Kola"));
+        stringMap1.put(3, Arrays.asList("Liuba", "Dima", "Andrey"));
+        stringMap1.put(2, Arrays.asList("E", "Anna", "Katya"));
+        stringMap1.put(1, Arrays.asList("Ludmila", "Anna", "Kira"));
+        metod(stringIntegerMap1, stringMap1);
 
     }
-    //10
-    public static void metod( Map<String,Integer>stringIntegerMap,Map<Integer,List<String>> integerListMap){
-        for(Map.Entry<String,Integer> map:stringIntegerMap.entrySet()){
 
+    //10
+    public static void metod(Map<String, Integer> stringIntegerMap, Map<Integer, List<String>> integerListMap) {
+        for (Map.Entry<String, Integer> map : stringIntegerMap.entrySet()) {
+            for (Map.Entry<Integer, List<String>> intMap : integerListMap.entrySet()) {
+                if ((map.getValue() == intMap.getKey()) && (checkForIdenticalLines(map.getKey(), intMap.getValue()))) {
+                    System.out.println(map + " and " + intMap);
+                }
+            }
         }
     }
+
+    public static boolean checkForIdenticalLines(String str, List<String> stringList) {
+
+        for (String listStr : stringList) {
+            if (str.equals(listStr)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     //9
-    public static List<String> keyValueConcatenation(Map<String,String> stringMap){
-        List<String> strings=new ArrayList<>();
+    public static List<String> keyValueConcatenation(Map<String, String> stringMap) {
+        List<String> strings = new ArrayList<>();
         StringBuilder stringBuilder;
-        for(Map.Entry<String,String>map: stringMap.entrySet()){
-            stringBuilder= new StringBuilder(map.getKey() + map.getValue());
+        for (Map.Entry<String, String> map : stringMap.entrySet()) {
+            stringBuilder = new StringBuilder(map.getKey() + map.getValue());
             strings.add(stringBuilder.toString());
         }
         return strings;
     }
 
     //8
-    public static void findingSubtractionKeyValue(Map<Integer,Integer> integerMap){
+    public static void findingSubtractionKeyValue(Map<Integer, Integer> integerMap) {
         String length;
-        for(Map.Entry<Integer,Integer>map:integerMap.entrySet()){
-         length=String.valueOf(map.getKey()-map.getValue());
-            if(length.length()==1){
-                System.out.println(map.getKey()+" and "+ map.getValue());
+        for (Map.Entry<Integer, Integer> map : integerMap.entrySet()) {
+            length = String.valueOf(map.getKey() - map.getValue());
+            if (length.length() == 1) {
+                System.out.println(map.getKey() + " and " + map.getValue());
             }
         }
     }
+
     //7
-    public  static Map<Integer,Integer> valueLengthStrings(Map<Integer,List<String>> integerListMap){
-        Map<Integer,Integer>newValueStringsLeugth=new HashMap<>();
-        for(Map.Entry<Integer,List<String>>map:integerListMap.entrySet() ){
+    public static Map<Integer, Integer> valueLengthStrings(Map<Integer, List<String>> integerListMap) {
+        Map<Integer, Integer> newValueStringsLeugth = new HashMap<>();
+        for (Map.Entry<Integer, List<String>> map : integerListMap.entrySet()) {
             int length = lengthAllStrings(map.getValue());
-            newValueStringsLeugth.put(map.getKey(),length);
+            newValueStringsLeugth.put(map.getKey(), length);
         }
         return newValueStringsLeugth;
     }
-    public static int lengthAllStrings(List<String>stringList){
-        int length =0;
-        for (String str:stringList){
-            length+=str.length();
+
+    public static int lengthAllStrings(List<String> stringList) {
+        int length = 0;
+        for (String str : stringList) {
+            length += str.length();
         }
         return length;
     }
 
     //6
-    public static Map<String,String>stringRepresentationNumberMultiplied2(Map<String,Integer>stringIntegerMap){
-        Map<String,String> stringMap=new HashMap<>();
-        for(Map.Entry<String,Integer> map:stringIntegerMap.entrySet()){
-         String newValue= String.valueOf((map.getValue()*2));
-         stringMap.put(map.getKey(),newValue);
+    public static Map<String, String> stringRepresentationNumberMultiplied2(Map<String, Integer> stringIntegerMap) {
+        Map<String, String> stringMap = new HashMap<>();
+        for (Map.Entry<String, Integer> map : stringIntegerMap.entrySet()) {
+            String newValue = String.valueOf((map.getValue() * 2));
+            stringMap.put(map.getKey(), newValue);
         }
-       return stringMap;
+        return stringMap;
     }
 
-//5
+    //5
     public static void keyReverseStringValue(Map<String, String> stringMap) {
         for (Map.Entry<String, String> str : stringMap.entrySet()) {
             if (examinationKeyReverseStringValue(str.getKey(), str.getValue())) {
@@ -179,20 +204,31 @@ public class Test {
         }
         return true;
     }
-//    public static void meaningsThatBeginWithVowel(Map<Integer,List<String>>integerListMap){
-//        for(Map.Entry<Integer,List<String>>map:integerListMap.entrySet()){
-//        }
-//
-//    }
-//    public static void checkFirstVowel(List<String>stringList){
-//        List<String> letterVowel=new ArrayList<>(Arrays.asList("A","O","U","E","I","Y"));
-//        for (String srt:stringList){
-//            if(letterVowel.get(0).startsWith(srt)){
-//
-//            }
-//        }
-//    }
-//2
+
+    //    3.
+    public static void meaningsThatBeginWithVowel(Map<Integer, List<String>> integerListMap) {
+        for (Map.Entry<Integer, List<String>> map : integerListMap.entrySet()) {
+            System.out.print("Key " + map.getKey() + " : ");
+            checkFirstVowel(map.getValue());
+
+        }
+
+    }
+
+    public static void checkFirstVowel(List<String> stringList) {
+        List<String> letterVowel = new ArrayList<>(Arrays.asList("A", "a", "O", "o", "U", "u", "E", "e", "I", "i", "Y", "y"));
+        for (String srt : stringList) {
+            for (String letter : letterVowel)
+                if (srt.startsWith(letter)) {
+                    System.out.print(srt + ", ");
+                    break;
+
+                }
+        }
+        System.out.println(" ");
+    }
+
+    //2
     public static int sumTheValuesAllKeysLetter(Map<String, Integer> numbers, char letter) {
         int sum = 0;
         for (Map.Entry<String, Integer> numb : numbers.entrySet()) {
@@ -203,7 +239,8 @@ public class Test {
         }
         return sum;
     }
-//4
+
+    //4
     public static void maximumKeyValue(Map<Integer, Integer> numbers) {
         int key = 0;
         int maxValue = Integer.MIN_VALUE;
@@ -217,8 +254,8 @@ public class Test {
 
     }
 
-   //1
-   public static void keyEqualsVolues(Map<String, String> stringMap) {
+    //1
+    public static void keyEqualsVolues(Map<String, String> stringMap) {
         int count = 0;
         for (Map.Entry<String, String> m : stringMap.entrySet()) {
             if (m.getValue().length() == m.getKey().length()) {
