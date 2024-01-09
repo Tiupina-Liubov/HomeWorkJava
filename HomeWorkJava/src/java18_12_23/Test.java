f284b63c6df65a05ddeaac6d0b402d210d56c477
Tiutina_Liubov <kyzjaxxx93@gmail.com>
2024-01-09 10:05:27 +0100

    18.12.23 Homework

--- HomeWorkJava/src/java18_12_23/Test.java
+++ HomeWorkJava/src/java18_12_23/Test.java
@@ -4,20 +4,47 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
+        System.out.println("Создайте функциональный интерфейс StringLength с методом getLength, который принимает строку и возвращает её длину.\n" +
+                "Напишите метод, который принимает строку и StringLength интерфейс и выводит длину строки.\n");
         System.out.println(lengthString(String::length, "String"));
+
+        System.out.println("Создайте функциональный интерфейс NumberOperation с методом operate, который принимает два целых числа и возвращает целое число.\n" +
+                "Напишите методы для выполнения базовых арифметических операций (сложение, вычитание, умножение, деление) с использованием этого интерфейса.\n");
         System.out.println(add(Integer::sum, 4, 6));
+
         System.out.println(sub((a, b) -> a - b, 4, 5));
+
         System.out.println(div((a, b) -> a / b, 6, 3));
+
         System.out.println(multi((a, b) -> a * b, 4, 5));
-        SafeDivision safeDivide = (a, b) -> b == 0 ? Optional.empty() : Optional.of(a / b);// Не понимаю как ето роботаэт
+
+        System.out.println("+Создайте функциональный интерфейс SafeDivision с методом divide, который безопасно делит два числа и возвращает Optional<Double>.\n" +
+                "Реализуйте метод, который делит два числа с использованием SafeDivision, обрабатывая случай деления на ноль.\n");
+        SafeDivision safeDivide = (a, b) -> b == 0 ? Optional.empty() : Optional.of(a / b);
         System.out.println("Результат: " + safeDivision(safeDivide, 20, 0).orElse(Double.NaN));
+
+        System.out.println("Создайте функциональный интерфейс UpperCaseConverter с методом convert, который преобразует строку в верхний регистр.\n" +
+                "Напишите метод, который принимает строку и UpperCaseConverter интерфейс, и преобразует её в верхний регистр.\n");
         System.out.println(upperCase(str -> str.toUpperCase(Locale.ROOT), "tan"));
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        System.out.println("Создайте функциональный интерфейс Logger с методом log, который принимает строку и выводит её в консоль.\n" +
+                "Напишите метод, который использует Logger для логирования различных сообщений.\n");
         logger(System.out::println, "hallo");
+
+        System.out.println("Создайте функциональный интерфейс ArrayTransformer с методом transform, который принимает массив\n" +
+                "целых чисел и выполняет операцию возведения каждого элемента в квадрат.\n" +
+                "Напишите метод, который принимает массив и ArrayTransformer, и изменяет массив, возведя каждый его элемент в квадрат.\n");
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         squaring(Test::squarings, numbers);
         System.out.println(Arrays.toString(numbers));
+
+        System.out.println("Создайте функциональный интерфейс TextAnalyzer с методом analyze, который принимает текст и анализирует его, возвращая результат анализа.\n" +
+                "Напишите метод, который принимает строку и TextAnalyzer, и подсчитывает количество уникальных слов в тексте.\n");
         int couchUniqueWords = numberUniqueWords(Test::wordsTheirNumber, "Hallo. How are you? You. you");
         System.out.println(couchUniqueWords);
+
+        System.out.println("Создайте функциональный интерфейс NumberProcessor с методом process, который выполняет операцию над массивом чисел.\n" +
+                "Напишите метод, который принимает массив чисел и NumberProcessor, и возвращает массив после умножения каждого элемента на -1\n");
         int[] numbersMulMinus1 = multiplyingEachElementMinus1(Test::multiply, numbers);
         System.out.println(Arrays.toString(numbersMulMinus1));
         multiply(numbersMulMinus1);
