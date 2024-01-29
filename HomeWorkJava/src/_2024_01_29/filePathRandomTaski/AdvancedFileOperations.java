952fe967dcb0d15b21c861a7d8ab8290c8be56c2
Tiupina Liubov <kyzjaxxx93@gmil.com>
2024-01-29 23:57:01 +0100

    task 29.01.24

---
+++ HomeWorkJava/src/_2024_01_29/filePathRandomTaski/AdvancedFileOperations.java
@@ -0,0 +1,47 @@
+package src._2024_01_29.filePathRandomTaski;
+
+import java.io.IOException;
+
+public class AdvancedFileOperations {
+
+    public static void main(String[] args) throws IOException {
+        countWordsInFile();
+        findTxtFilesInDirectory();
+        copyLastParagraphUsingRandomAccess();
+    }
+
+    // Усложненное Задание 1: Работа с классом File
+
+    /**
+     * Написать метод, который читает файл "example.txt",
+     * подсчитывает количество слов в файле и записывает это количество
+     * в новый файл "word_count.txt". Использовать BufferedReader и BufferedWriter
+     * для эффективной работы с текстом.
+     */
+    public static void countWordsInFile() throws IOException {
+
+
+    }
+
+    // Усложненное Задание 2: Работа с классом Path
+
+    /**
+     * Написать метод, который ищет все файлы с расширением ".txt"
+     * в текущей директории и её поддиректориях. Результаты поиска
+     * (пути к файлам) записать в файл "found_files.txt".
+     */
+    public static void findTxtFilesInDirectory() throws IOException {
+
+    }
+
+    // Усложненное Задание 3: Работа с классом RandomAccessFile
+
+    /**
+     * Написать метод, который открывает файл "example.txt"
+     * с использованием RandomAccessFile, переходит к последнему абзацу
+     * в файле и копирует его содержимое в новый файл "last_paragraph.txt".
+     */
+    public static void copyLastParagraphUsingRandomAccess() throws IOException {
+
+    }
+}
